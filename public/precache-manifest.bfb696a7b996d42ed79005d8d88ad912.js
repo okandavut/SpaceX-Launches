fc3ad82e0e01632a39fbebf4d54e0f6c8428a52a
okandavutcom <davut.okan@gmail.com>
2019-05-02 01:36:20 +0300

    Initial Commit

---
+++ public/precache-manifest.bfb696a7b996d42ed79005d8d88ad912.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea7509eae59387bab2820ed6493e7eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45f7db74611bfc39e4b0",
+    "url": "/static/css/main.e23fee82.chunk.css"
+  },
+  {
+    "revision": "c442848abe14bd50f193",
+    "url": "/static/js/2.93de258a.chunk.js"
+  },
+  {
+    "revision": "45f7db74611bfc39e4b0",
+    "url": "/static/js/main.aea9019f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const cors = require("cors");
+const schema = require("./schema");
+const path = require("path");
+
+const app = express();
+
+//Allow cros-origin
+app.use(cors());
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true // if we use client to make graphql queries for server
+  })
+);
+
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
